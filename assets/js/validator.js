a26e367527c1b8f416c220666b775d5968341114
Thuc Nguyen <johnthuc1997@gmail.com>
2021-05-09 14:52:42 +0700

    update

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -1,85 +1,90 @@
 const $ = document.querySelector.bind(document);
 
-const Validator = {
-  run(options) {
-    let eForm = $(options.form);
-    if (eForm) {
-      options.rules.forEach(rule => {
-        let eInput = $(rule.selector);
-        if (eInput) {
-          eInput.onblur = (e) => {
-            this.validateCheck(e.target, rule);
-          };
-          eInput.oninput = (e) => {
-            this.validateCheck(e.target, rule);
-          };
-        }
-      });
-    }
+const ValidatorRules = {
+  isRequired: (msg = 'Vui lòng nhập trường này!') => {
+    return {
+      test(value) {
+        return (value === '' ? msg : undefined);
+      }
+    };
   },
-  validateSetError(e, value) {
-    e.nextElementSibling && (e.nextElementSibling.innerHTML = value);
+
+  isEmail: (msg = 'Email không hợp lệ!') => {
+    return {
+      test(value) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return (!re.test(String(value).toLowerCase()) ? msg : undefined);
+      }
+    };
   },
-  validateActive(e) {
-    e.parentElement && e.parentElement.classList.add('invalid');
+
+  maxLength: (max, msg = 'Độ dài chuỗi vượt quá giới hạn') => {
+    return {
+      test(value) {
+        return (value.length > max ? msg : undefined);
+      }
+    };
   },
-  validateRemove(e) {
-    e.parentElement && e.parentElement.classList.remove('invalid');
+
+  minLength: (min, msg = 'Độ dài chuỗi tối thiểu không đủ') => {
+    return {
+      test(value) {
+        return (value.length < min ? msg : undefined);
+      }
+    };
   },
-  validateCheck(e, rule) {
-    let msgError = rule.test(e.value.trim());
-    if (msgError) {
-      this.validateActive(e);
-      this.validateSetError(e, msgError);
-    } else {
-      this.validateSetError(e, '');
-      this.validateRemove(e);
-    }
-  }
-};
 
-Validator.isRequired = (selector, msg = 'Vui lòng nhập trường này!') => {
-  return {
-    selector,
-    test(value) {
-      return value === '' ? msg : undefined;
-    }
-  };
-};
-
-Validator.isEmail = (selector, msg = 'Email không hợp lệ!') => {
-  return {
-    selector,
-    test(value) {
-      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return !re.test(String(value).toLowerCase()) ? msg : undefined
-    }
-  };
-};
-
-Validator.maxLength = (selector, max, msg = 'Độ dài chuỗi vượt quá giới hạn') => {
-  return {
-    selector,
-    test(value) {
-      return value.length > max ? msg : undefined;
-    }
-  };
-};
-
-Validator.minLength = (selector, min, msg = 'Độ dài chuỗi tối thiểu không đủ') => {
-  return {
-    selector,
-    test(value) {
-      return value.length < min ? msg : undefined;
-    }
-  };
-};
-
-Validator.isConfirmed = (selector, getConfirmValue, msg = 'Giá trị nhập vào không trùng khớp') => {
-  return {
-    selector,
-    test(value) {
-      return value === getConfirmValue() ? undefined : msg;
+  isConfirmed: (callback, msg = 'Giá trị nhập vào không trùng khớp') => {
+    return {
+      test(value) {
+        return (value === callback() ? undefined : msg);
+      }
     }
   }
 }
+
+const Validator = {
+
+  run(options) {
+    let eForm = $(options.form);
+    if (eForm) {
+      let rules = options.rules;
+      for (const selector in rules) {
+        let rulesSelector = rules[selector];
+        console.group(selector);
+        console.log(rulesSelector);
+        console.groupEnd();
+        let eInput = $(selector);
+        if (eInput) {
+          eInput.onblur = () => {
+            this.checkError(rulesSelector, eInput.value.trim(), msgError => {
+              this.setError(eInput, msgError);
+            });
+          };
+          eInput.oninput = () => {
+            this.checkError(rulesSelector, eInput.value.trim(), msgError => {
+              this.setError(eInput, msgError);
+            });
+          };
+        }
+      }
+    }
+  },
+  checkError(rules, value, callback) {
+    let msgError = '';
+    rules.some(rule => {
+      let key = Object?.keys(rule)[0];
+      return (msgError = ValidatorRules[key](...rule[key]).test(value));
+    });
+    callback(msgError);
+  },
+  setError(e, msgError) {
+    if (msgError) {
+      e.parentElement && e.parentElement.classList.add('invalid');
+      e.nextElementSibling && (e.nextElementSibling.innerHTML = msgError);
+    } else {
+      e.nextElementSibling && (e.nextElementSibling.innerHTML = '');
+      e.parentElement && e.parentElement.classList.remove('invalid');
+    }
+  }
+};
